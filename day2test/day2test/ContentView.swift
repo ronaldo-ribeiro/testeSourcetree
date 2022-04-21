31e73c9268b947ced721a3799cbe6a8ba987710f
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-04-21 12:07:40 -0300

    change on 12h
    change on 12h

---
+++ day2test/day2test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day2test
+//
+//  Created by Ronaldo Ribeiro on 21/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Testando as funcionalidades do Git")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day2test/day2test/day2testApp.swift
@@ -0,0 +1,17 @@
+//
+//  day2testApp.swift
+//  day2test
+//
+//  Created by Ronaldo Ribeiro on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct day2testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
